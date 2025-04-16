09989bf6c2d07ad64ccb23d43cb937636a5cc335
Srandom1 <Ma1nmailAND@gmail.com>
2025-04-16 11:41:28 +0300

    создание анимаций

--- game/viewModels/abstract_view.py
+++ game/viewModels/abstract_view.py
@@ -1,14 +1,36 @@
 from abc import ABC, abstractmethod
 
+from PyQt5.QtCore import QRect
+from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import *
 
-class CustomView(ABC, QWidget):
-    
-    
+
+class CustomView(QWidget):
+
     def __init__(self):
         super().__init__()
-        self.child = []
+        self.childes = []
+        self._icon_x_offset = 0
+        self._icon_y_offset = 0
+        self.icon = None
     
-    def paint(self):
-        pass
+    def paint_recursively(self):
+        """Служит для отрисовки всех дочерних элементов, включая отрисовки самого элемента"""
+        self.childes: list[CustomView]
+        self.paintEvent(None)
+        for child in self.childes:
+            child.paintEvent(None)
 
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        if not self.icon.isNull():
+            # Рисуем иконку по центру виджета
+            target_rect = QRect(
+                (self.width() - self.icon.width()) // 2,
+                (self.height() - self.icon.height()) // 2,
+                self.icon.width(),
+                self.icon.height()
+            )
+            painter.drawPixmap(target_rect, self.icon)
+
+    def animate(self):
--- game/viewModels/view_models.py
+++ game/viewModels/view_models.py
@@ -1,12 +1,21 @@
 from enum import Enum
 
+from PyQt5.QtGui import QPainter, QPixmap
+from PyQt5.QtCore import QRect
+
 from game.viewModels.abstract_view import *
 from game.models import models
 from game.utils import *
 
+REDACTED_IMAGE_PATH = IMAGES_PATH + "/"
+
 
 class CellIcons(Enum):
     """Каждое значение - словарь иконок (именно словарь, так как возможно придется делать анимации)"""
+    player = QPixmap(REDACTED_IMAGE_PATH + "player.png")
+    wall = QPixmap(REDACTED_IMAGE_PATH + "wall.png")
+    floor = QPixmap(REDACTED_IMAGE_PATH + "floor.png")
+    finish_mark = QPixmap(REDACTED_IMAGE_PATH + "finish_mark.png")
 
 
 class CellView(CustomView):
@@ -15,6 +24,23 @@ class CellView(CustomView):
         super().__init__()
 
 
+class BoxView(CustomView):
+
+    def __init__(self):
+        super().__init__()
+
+
+class PlayerView(CustomView):
+
+    def __init__(self):
+        super().__init__()
+
+
+class FinishMarkView:
+
+    def __init__(self):
+        super().__init__()
+
+
 if __name__ == "__main__":
     print(IMAGES_PATH)
-
