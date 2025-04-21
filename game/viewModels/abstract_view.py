4986f62a561398f2ccaf80bcd6aaf6ec5e519f91
Srandom1 <Ma1nmailAND@gmail.com>
2025-04-21 16:10:09 +0300

    ....

--- game/viewModels/abstract_view.py
+++ game/viewModels/abstract_view.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from game.models.models import *
 
 from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import *
-
+from PyQt5.QtCore import QTimer
 
 class CustomView(QWidget):
 
@@ -13,7 +14,12 @@ class CustomView(QWidget):
         self._icon_x_offset = 0
         self._icon_y_offset = 0
         self.icon = None
-    
+
+        self.animation_timer = QTimer(self)
+        self.animation_timer.timeout.connect(self._perform_animation)
+        self.y_delta = 0
+        self.x_delta = 0
+
     def paint_recursively(self):
         """Служит для отрисовки всех дочерних элементов, включая отрисовки самого элемента"""
         self.childes: list[CustomView]
@@ -26,11 +32,23 @@ class CustomView(QWidget):
         if not self.icon.isNull():
             # Рисуем иконку по центру виджета
             target_rect = QRect(
-                (self.width() - self.icon.width()) // 2,
-                (self.height() - self.icon.height()) // 2,
+                (self.width() - self.icon.width()) // 2 + self._icon_x_offset,
+                (self.height() - self.icon.height()) // 2 + self._icon_y_offset,
                 self.icon.width(),
                 self.icon.height()
             )
             painter.drawPixmap(target_rect, self.icon)
 
-    def animate(self):
+    def animate(self, direction: Direction):
+        self.x_delta = 0
+        self.y_delta = 0
+
+
+        self.animation_timer.start()
+
+    def _perform_animation(self):
+        if self._icon_x_offset < self.target_offset_x:
+            self._icon_x_offset += self.animation_step
+            self.update()
+        else:
+            self.animation_timer.stop()
