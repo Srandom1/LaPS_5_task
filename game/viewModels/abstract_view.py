b20078c40842e2237a13e79d425495a6c81f4b0c
Srandom1 <100489531+Srandom1@users.noreply.github.com>
2025-04-23 02:37:55 +0300

    Штирлец посмотрел в биноколь на линию фронта. "Дед лайн" - подумал Штирлец.

--- game/viewModels/abstract_view.py
+++ game/viewModels/abstract_view.py
@@ -6,19 +6,35 @@ from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QTimer
 
-class CustomView(QWidget):
 
+class CustomView(QWidget):
+    # TODO При анимации персонажа подумать, как делать перепивязку текущего объекта к другому, возможно
+    #  придется занулять параметры анимации, в частности текущее смещение
     def __init__(self):
         super().__init__()
         self.childes = []
+        #Связаная с объектом модель
+        self.linked_model = None
         self._icon_x_offset = 0
         self._icon_y_offset = 0
         self.icon = None
 
-        self.animation_timer = QTimer(self)
-        self.animation_timer.timeout.connect(self._perform_animation)
-        self.y_delta = 0
-        self.x_delta = 0
+        self._animation_timer = QTimer(self)
+        self._animation_timer.timeout.connect(self._perform_animation)
+        self._y_delta = 0
+        self._x_delta = 0
+        self._target_offset_x = 0
+        self._target_offset_y = 0
+
+        self._modulus_increment = 3
+
+    @property
+    def modulus_increment(self):
+        return self._modulus_increment
+
+    @modulus_increment.setter
+    def modulus_increment(self, value):
+        self._modulus_increment = abs(value)
 
     def paint_recursively(self):
         """Служит для отрисовки всех дочерних элементов, включая отрисовки самого элемента"""
@@ -40,15 +56,39 @@ class CustomView(QWidget):
             painter.drawPixmap(target_rect, self.icon)
 
     def animate(self, direction: Direction):
-        self.x_delta = 0
-        self.y_delta = 0
-
-
-        self.animation_timer.start()
+        """Анимация в текущей ее реализации не предполагает смещение по двум осям координат сразу.
+        при передачи направления устанавливается некоторый целевой отступ и шаг"""
+        self._x_delta = 0
+        self._y_delta = 0
+        # не придумал как лучше сделать, так что терпите ifки
+        if direction == Direction.UP:
+            self._y_delta = -abs(self.modulus_increment)
+            self._target_offset_y = -self.icon.height()
+        elif direction == Direction.DOWN:
+            self._y_delta = -abs(self.modulus_increment)
+            self._target_offset_y = self.icon.height()
+        elif direction == Direction.LEFT:
+            self._x_delta = -abs(self.modulus_increment)
+            self._target_offset_x = -self.icon.height()
+        elif direction == Direction.RIGHT:
+            self._x_delta = abs(self.modulus_increment)
+            self._target_offset_x = self.icon.height()
+        else:
+            # Вот это на случае если нам передали какую-то хрень
+            return
+        self._animation_timer.start()
 
     def _perform_animation(self):
-        if self._icon_x_offset < self.target_offset_x:
-            self._icon_x_offset += self.animation_step
-            self.update()
+        """Запускает анимацию в соответствии с заданными параметрами анимации такие, как приращение по оси и целевое
+         смещение"""
+        # Модуль берется из-за того, что движение может быть в отрицательном направлении.
+        if abs(self._icon_x_offset) < abs(self._target_offset_x) and self._x_delta != 0:
+            self._icon_x_offset += self._x_delta
+        elif abs(self._icon_y_offset) < abs(self._target_offset_y) and self._y_delta != 0:
+            self._icon_y_offset += self._icon_y_offset
         else:
-            self.animation_timer.stop()
+            # Во избежания неожиданного поведения при запуске новой анимации для объекта.
+            self._x_delta = 0
+            self._y_delta = 0
+            self._animation_timer.stop()
+        self.update()
--- game/viewModels/view_models.py
+++ game/viewModels/view_models.py
@@ -10,18 +10,49 @@ from game.utils import *
 REDACTED_IMAGE_PATH = IMAGES_PATH + "/"
 
 
-class CellIcons(Enum):
-    """Каждое значение - словарь иконок (именно словарь, так как возможно придется делать анимации)"""
-    player = QPixmap(REDACTED_IMAGE_PATH + "player.png")
-    wall = QPixmap(REDACTED_IMAGE_PATH + "wall.png")
-    floor = QPixmap(REDACTED_IMAGE_PATH + "floor.png")
-    finish_mark = QPixmap(REDACTED_IMAGE_PATH + "finish_mark.png")
+def generate_icons_dict_template():
+    return {"default": None}
+
+
+class Icons:
+    """Каждое значение - словарь иконок (именно словарь, так как возможно придется делать анимации основанные на замене картинок)
+    такое своеобразное создание иконок в 2 этапе связано с особенностями работы с Qt"""
+    player = generate_icons_dict_template()
+    wall = generate_icons_dict_template()
+    floor = generate_icons_dict_template()
+    finish_mark = generate_icons_dict_template()
+
+    @classmethod
+    def set_icons(cls):
+        cls.player["default"] = QPixmap(REDACTED_IMAGE_PATH + "player.png")
+        cls.wall["default"] = QPixmap(REDACTED_IMAGE_PATH + "wall.png")
+        cls.floor["default"] = QPixmap(REDACTED_IMAGE_PATH + "floor.png")
+        cls.finish_mark["default"] = QPixmap(REDACTED_IMAGE_PATH + "finish_mark.png")
+
+
+class FinishMarkView(CustomView):
+
+    def __init__(self):
+        super().__init__()
 
 
 class CellView(CustomView):
 
-    def __init__(self):
+    """
+    Ух ну тут прикол с переопределением сигнатуры конструктора, причем добавлением обязательного аргумента, что делать
+    нельзя, но я не придумал как лаконично это сделать, так что сорян. Можно, конечно, в условной метке фективно сувать
+    туда None, может быть так и сделаю, но пока что так"""
+    def __init__(self, cell: Cell):
         super().__init__()
+        self.linked_model = cell
+        if cell.cell_status != CellStatus.DISABLE:
+            self.icon = Icons.floor
+        else:
+            self.icon = Icons.wall
+        if cell.cell_status == CellStatus.FINISH:
+            mark = FinishMarkView()
+            # Важно в дальнейшем следить, что бы метка была последним ребенком у объекта, иначе ее просто перекроют
+            self.childes.append(mark)
 
 
 class BoxView(CustomView):
@@ -31,12 +62,6 @@ class BoxView(CustomView):
 
 
 class PlayerView(CustomView):
-
-    def __init__(self):
-        super().__init__()
-
-
-class FinishMarkView:
 
     def __init__(self):
         super().__init__()
